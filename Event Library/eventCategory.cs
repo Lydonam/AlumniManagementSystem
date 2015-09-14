e2e63bfbc52f6fa4969f8640fc5b856121cdb37e
colvinc2 <tud13239@CIS-SE20426.tu.temple.edu>
2015-09-14 12:07:26 -0400

    budget , event category, and event overview classes created

---
+++ Event Library/eventCategory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Library
+{
+    public class eventCategory
+    {
+        private Boolean alumniSociety { get; set; }
+        private Boolean athletics { get; set; }
+        private Boolean career { get; set; }
+        private Boolean cultural { get; set; }
+        private Boolean educational { get; set; }
+        private Boolean networking { get; set; }
+        private Boolean regionalChapter { get; set; }
+        private Boolean reunion { get; set; }
+        private Boolean schoolAndCollege { get; set; }
+        private Boolean student { get; set; }
+        private Boolean volunteer { get; set; }
+        private Boolean webinar { get; set; }
+    }
+}
---
+++ Event Library/eventOverview.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Library
+{
+    public class eventOverview
+    {
+        private String goal { get; set; }
+        private String eventURL { get; set; }
+        private float fees { get; set; }
+        private int attendeeCapacity { get; set; }
+        private int preEventRegNumber { get; set; }
+        private int postEventRegNumber { get; set; }
+        
+    }
+}
