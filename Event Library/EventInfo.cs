2a50aa9df1fb8b5446ee2ea9408c027b7fbc6c69
colvinc2 <tud13239@CIS-SE20426.tu.temple.edu>
2015-09-14 11:29:08 -0400

    Event info and event sponsor classes created

---
+++ Event Library/EventInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Library
+{
+    public class EventInfo
+    {
+        private String eventName { get; set; }
+        private String eventOwner { get; set; }
+        private String streetAddress1 { get; set; }
+        private String streetAddress2 { get; set; }
+        private String roomNumber { get; set; }
+        private DateTime startDate { get; set; }
+        private DateTime endDate { get; set; }
+        private TimeSpan startTime { get; set; }
+        private TimeSpan endTime { get; set; }
+        private int zipcode { get; set; }
+        private String city { get; set; }
+        private String country { get; set; }
+        private String timeZone1 { get; set; }
+        private String timeZone2 { get; set; }
+        private String buildingName { get; set; } 
+
+    }
+}
---
+++ Event Library/eventSponsor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Library
+{
+    public class eventSponsor
+    {
+        private String sponsorName { get; set; }
+        private String sponsorEmail { get; set; }
+        private String cosponsorName { get; set; }
+        private String cosponsorEmail { get; set; }
+        private Boolean universityPartner { get; set; }
+    }
+}
